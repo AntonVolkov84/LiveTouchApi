01e95662273a1d2ee7a64ab52a2156562c1193f9
root <root@jfofovnfrg.local>
2025-12-11 15:40:25 +0000

    prepare for repair minio

--- db/db.js
+++ db/db.js
@@ -17,7 +17,7 @@ async function createTable() {
     await client.query(`
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
+        username VARCHAR(50) NOT NULL,
         usersurname VARCHAR(50) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
         password_hash TEXT NOT NULL,
